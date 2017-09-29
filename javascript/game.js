e44af56579f39316f25fef3ba1061e55c4e5afb1
deep-research <deep-research>
2017-09-29 15:26:32 -0500

    Counter Initialization

--- javascript/game.js
+++ javascript/game.js
@@ -14,6 +14,7 @@ var guessCount = document.getElementById("guess-count");
 var guessHistory = document.getElementById("guess-history");
 var winWrite = document.getElementById("wins");
 var lossWrite = document.getElementById("losses");
+guessCount.innerHTML = guessesLeft;
 
 function randLetter() {
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
